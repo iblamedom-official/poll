8e059f4dd25d7c88db4b945016625db6aec9b9f9
iblamedom <158624575+iblamedom-official@users.noreply.github.com>
2024-02-14 23:12:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/ Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
 
